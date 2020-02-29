38268204a9794845214aad78643510c043b27fe1
verneshahazel <60764595+verneshahazel@users.noreply.github.com>
2020-02-29 12:36:42 -0600

    update

--- 412ProjectFINAL.py
+++ 412ProjectFINAL.py
@@ -1,4 +1,3 @@
-  
 #imports
 import urllib.request
 
@@ -8,6 +7,12 @@ urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg476/http_access_log", "
 with open("412File.txt", "r") as File:
     Count=0
     LocationList=[]
+    DashList1=[]
+    DashList2=[]
+    DateList=[]
+    TimeZoneList=[]
+    GETList=[]
+    
     for line in File:
         Line=line.split(" ")
         
@@ -15,11 +20,27 @@ with open("412File.txt", "r") as File:
         Location=Line[0]
         Dash=Line[1]
         Dash2=Line[2]
+        Date=Line[3] #Come back and remove leading " [ "
+        TimeZone=Line[4] #Come back and remove ending " ] "
+        GET=Line[5]
         
+        #append split data to it's respective list
         
         LocationList.append(Location)
-        while Count<5:
+        DashList1.append(Dash)
+        DashList2.append(Dash2)
+        DateList.append(Date)
+        TimeZoneList.append(TimeZone)
+        GETList.append(GET)
+        
+        while Count<5: #This while loop is just so that it stops trying to run the whole data set because that's stressing my poor computer out
             print(Location)
             print(Dash)
             print(Dash2)
+            print(Date)
+            print(TimeZone)
+            print(GET)
             Count+=1
+    
+    print()
+    print(f" There were {Count} total requests made in the time period requested in the log.")
